48b91f5092fc7d65077ebe0a1880e978c295f721
Menchu MV <mcmvillalobos@gmail.com>
2021-10-18 19:17:03 +0200

    Took actions and variables outside the icon component

--- src/components/icons/icon.js
+++ src/components/icons/icon.js
@@ -1,20 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
-import { faThumbsDown } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
 
-function Icon() {
-  const [thumbsUp, setThumbsUp] = useState(true);
-  function toggle() {
-    setThumbsUp(!thumbsUp);
-  }
+function Icon({ icon, actionOnClick }) {
   return (
     <>
-      <FontAwesomeIcon
-        icon={thumbsUp ? faThumbsUp : faThumbsDown}
-        onClick={toggle}
-        size="6x"
-      />
+      <FontAwesomeIcon icon={icon} onClick={actionOnClick} size="6x" />
     </>
   );
 }
